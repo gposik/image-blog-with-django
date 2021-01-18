263404137d40ac6a333b39a6506ed2ee70f4e78b
Gastón <gposik@gmail.com>
2021-01-18 02:32:32 -0300

    Added background color variable to Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 
 class Post (models.Model):
 
+    COLOR_CHOICES = (
+        ('#00A056', 'Verde bosque'),
+        ('#FFA1A1', 'Rosa'),
+        ('#9CD484', 'Verde manzana'),
+        ('#FDAE00', 'Amarillo'),
+        ('#68735D', 'Musgo'),
+    )
+
     title = models.CharField(max_length=250)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", blank=True)
+    body = RichTextField(blank=True, null=True)
+    background_color = models.CharField(
+        max_length=30, choices=COLOR_CHOICES, default='Verde bosque')
     created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=250, blank=True, unique_for_date='created')
 
