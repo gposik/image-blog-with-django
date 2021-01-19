b1070bbe0cf3d62d09308bc286993530fe358faf
Gastón <gposik@gmail.com>
2021-01-19 20:55:22 -0300

    Added background-image div to get mosaic effect on posts

--- mysite/blog/static/js/app.js
+++ mysite/blog/static/js/app.js
@@ -1,8 +1,6 @@
-
 const toggleBtnOpen = document.querySelector('#menu-icon-open');
 
 const toggleBtnClose = document.querySelector('#menu-icon-close');
-
 
 const divList = document.querySelector('.menu-content');
 
@@ -23,3 +21,24 @@ toggleBtnClose.addEventListener("click", () => {
     divList.style.display = "none";
   }
 });
+
+
+// 
+
+
+
+// const post = document.querySelector('.post-wrapper');
+
+// const arrow = document.querySelector('#arrow');
+
+// arrow.addEventListener('click', () => {
+	
+// 	let url = window.location.href;
+// 	let splitted_url = url.split('/');
+// 	console.log(splitted_url);
+// 	let post_id = splitted_url.slice(-1)[0];
+// 	console.log(post_id);
+// 	arrow.href = post_id;	
+// 	console.log(arrow.href);
+
+// })
